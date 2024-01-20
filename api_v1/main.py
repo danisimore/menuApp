1fc4738a4b71847658bff3e1846f0a7e460ec5ed
danisimore <danisimore@yandex.ru>
2024-01-21 00:33:46 +0300

    Добавлен метод для получения submenu для определенного меню

--- api_v1/main.py
+++ api_v1/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from menu.router import router as menu_router
+from submenu.router import router
 
 app = FastAPI(
     title="Restaurant Menu"
 )
 
 app.include_router(menu_router)
-
+app.include_router(router)
