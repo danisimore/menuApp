7d8a2291b70e75077f6df0e0105584425b8c6e4a
danisimore <danisimore@yandex.ru>
2024-02-06 20:17:58 +0300

    Вынесена лишняя логика из ручек меню

--- api_v1/menu/menu_services.py
+++ api_v1/menu/menu_services.py
@@ -122,3 +122,20 @@ async def delete_menu(
     await session.execute(stmt)
 
     await session.commit()
+
+
+async def parse_menu_data(menu_data: list[tuple[Menu, int, int]]) -> Menu | None:
+    """
+    Парсит полученный список с кортежем, в котором хранится Объект, счетчик подменю и блюд
+
+    :param menu_data: список с кортежем, в котором хранится Объект, счетчик подменю и блюд
+    :return: если в ходе выборки нашлось меню, то меню с добавленными атрибутами submenus_count и dishes_count
+    """
+    if menu_data:
+        menu = menu_data[0][0]
+        menu.submenus_count = menu_data[0][1]
+        menu.dishes_count = menu_data[0][2]
+
+        return menu
+
+    return None
